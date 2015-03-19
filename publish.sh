2a56b6c7b2c298e172fbe2fec6d3d07128d025c2
Orko <orquito@gmail.com>
2015-03-19 07:45:49 -0300

    Modifico el script de publis.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "instalando bamboo architecture"
-grails clean && grails refresh-dependencies && grails publish-plugin
+grails clean && grails refresh-dependencies && grails maven-install
 
 #grails publish-plugin 
 
