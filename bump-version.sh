3e6573fddc517ce145ff4505bad741a2c7249993
Orko <orquito@gmail.com>
2015-07-01 06:33:37 -0300

    hot-fix-v1.0.0: Cambio el archivo de bump-version para que no tagee y no comitee. Eso se hace en el master

--- bump-version.sh
+++ bump-version.sh
@@ -37,8 +37,8 @@ if [ -f VERSION ]; then
     mv tmpfile CHANGES
     git add CHANGES VERSION
     git commit -m "Version bump to $INPUT_STRING"
-    git tag -a -m "Tagging version $INPUT_STRING" "v$INPUT_STRING"
-    git push origin --tags
+#    git tag -a -m "Tagging version $INPUT_STRING" "v$INPUT_STRING"
+ #   git push origin --tags
 else
     echo "Could not find a VERSION file"
     read -p "Do you want to create a version file and start from scratch? [y]" RESPONSE
@@ -55,8 +55,8 @@ else
         echo "" >> CHANGES
         git add VERSION CHANGES
         git commit -m "Added VERSION and CHANGES files, Version bump to v0.1.0"
-        git tag -a -m "Tagging version 0.1.0" "v0.1.0"
-        git push origin --tags
+  #      git tag -a -m "Tagging version 0.1.0" "v0.1.0"
+   #     git push origin --tags
     fi
     
 fi
